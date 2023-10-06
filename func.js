d7684bf3ddaf760178c81c51966fd83d0b642f1e
Phuwadon Decharam <phuwadon_dev@hotmail.com>
2023-10-06 20:13:00 +0700

    function calMyAge

---
+++ func.js
@@ -0,0 +1,5 @@
+var calculateMyAge = function (birthYear) {
+    return 2023 - birthYear;
+};
+var myAge = calculateMyAge(2000);
+console.log(myAge);
---
+++ func.ts
@@ -0,0 +1,6 @@
+const calculateMyAge = (birthYear:number) => {
+    return 2023-birthYear;
+}
+
+const myAge : number = calculateMyAge(2000);
+console.log(myAge)
