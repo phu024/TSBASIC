d1fa5c83c5fd3a73b43a7dbc6aa2da85f7e8bb73
Phuwadon Decharam <phuwadon_dev@hotmail.com>
2023-10-06 20:03:03 +0700

    object

---
+++ app.js
@@ -0,0 +1,14 @@
+var user = {
+    name: "Jarry",
+    age: 22,
+    isMarried: false,
+    hubbies: ["Playing Football", "Watching Movies"]
+};
+console.log(user);
+var newUser = {
+    name: "John Doe",
+    age: "28",
+    isMarried: false,
+    hubbies: ["Playing Football", "Watching Movies"]
+};
+console.log(user);
---
+++ app.ts
@@ -0,0 +1,22 @@
+let user: {
+    name: string,
+    age: number,
+    isMarried: boolean,
+    hubbies: string[]
+} = {
+    name: "Jarry",
+    age: 22,
+    isMarried: false,
+    hubbies: ["Playing Football", "Watching Movies"]
+}
+
+console.log(user);
+
+user = {
+    name: "John Doe",
+    age: 28,
+    isMarried: false,
+    hubbies: ["Playing Football", "Watching Movies"]
+}
+
+console.log(user);
