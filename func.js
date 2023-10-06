6e054ba94e54544ba545b073faa9523e0b79fb21
Phuwadon Decharam <phuwadon_dev@hotmail.com>
2023-10-06 20:27:12 +0700

    function

--- func.js
+++ func.js
@@ -3,3 +3,10 @@ var calculateMyAge = function (birthYear) {
 };
 var myAge = calculateMyAge(2000);
 console.log(myAge);
+function greetMe(userInfo) {
+    return (userInfo.name +
+        ". It's nice to know that you are " +
+        userInfo.age +
+        " years old.");
+}
+console.log(greetMe({ name: "John Doe", age: calculateMyAge(2000) }));
--- func.ts
+++ func.ts
@@ -4,3 +4,14 @@ const calculateMyAge = (birthYear:number) :number => {
 
 const myAge : number = calculateMyAge(2000);
 console.log(myAge)
+
+function greetMe(userInfo : {name:string, age : number}): string{
+    return (
+        userInfo.name +
+        ". It's nice to know that you are " +
+        userInfo.age +
+        " years old."
+    );
+}
+
+console.log(greetMe({name:"John Doe",age:calculateMyAge(2000)}));
